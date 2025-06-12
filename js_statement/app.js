2fcc4188229ba0631c2a80b83360a8f5f4c26f85
Abu Ubaidah <ubaidahabu658@gmail.com>
2025-06-12 15:06:25 -0700

    Add files via upload

---
+++ js_statement/app.js
@@ -0,0 +1,17 @@
+console.log('Hello');
+
+document.getElementById('title').innerHTML = 'Hello World';
+
+let sum = 150 + 40;
+console.log(sum);
+
+if (sum > 100) {
+  console.log('Expensive');
+} else {
+  console.log('Cheep');
+}
+
+function sayHello(name) {
+  console.log(`Hello ${name}`);
+}
+sayHello('rakib');
